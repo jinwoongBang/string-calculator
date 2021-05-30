630e4c4149fb49e373735c9996dbddebfec1e8e2
jinwoong-bang <jwbang@malgn.com>
2021-05-30 18:11:01 +0900

    [2] 테스트에서 공통으로 사용하는객체를 멤버 변수로 분리" git push

--- src/test/java/_02_calculator/CalculatorTest.java
+++ src/test/java/_02_calculator/CalculatorTest.java
@@ -1,13 +1,24 @@
 package _02_calculator;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
 
+  private Calculator cal;
+
+  @Before
+  public void setup() {
+    cal = new Calculator();
+  }
+
   @Test
   public void add() {
-    Calculator cal = new Calculator();
     assertEquals(9, cal.add(6, 3));
+  }
+  @Test
+  public void subtract() {
+    assertEquals(3, cal.subtract(6, 3));
   }
 }
